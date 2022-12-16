27d4b6bd56ebcea22b107d08748b6741fb2c41e1
Phillip Zeba <zeba@student.tugraz.at>
2022-12-16 14:16:53 +0100

    this time fr fr

--- gui/src/main/java/at/tugraz/oop2/gui/FractalApplication.java
+++ gui/src/main/java/at/tugraz/oop2/gui/FractalApplication.java
@@ -374,7 +374,7 @@ public class FractalApplication extends Application {
         TextField mandelbrotXTextField = new TextField(Double.toString(mandelbrotX.get()));
         mandelbrotXTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                Pattern pattern = Pattern.compile("-?[0-9](\\.)");
+                Pattern pattern = Pattern.compile("-?[0-9](\\.)?");
                 Matcher matcher = pattern.matcher(newValue.toString());
                 boolean matchFound = matcher.matches();
                 if (!matchFound) {
@@ -391,7 +391,7 @@ public class FractalApplication extends Application {
         TextField mandelbrotYTextField = new TextField(Double.toString(mandelbrotY.get()));
         mandelbrotYTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                Pattern pattern = Pattern.compile("-?[0-9](\\.)");
+                Pattern pattern = Pattern.compile("-?[0-9](\\.)?");
                 Matcher matcher = pattern.matcher(newValue.toString());
                 boolean matchFound = matcher.matches();
                 if (!matchFound) {
@@ -408,7 +408,7 @@ public class FractalApplication extends Application {
         TextField mandelbrotZoomTextField = new TextField(Double.toString(mandelbrotZoom.get()));
         mandelbrotZoomTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                Pattern pattern = Pattern.compile("-?[0-9](\\.)");
+                Pattern pattern = Pattern.compile("-?[0-9](\\.)?");
                 Matcher matcher = pattern.matcher(newValue.toString());
                 boolean matchFound = matcher.matches();
                 if (!matchFound) {
@@ -424,7 +424,7 @@ public class FractalApplication extends Application {
         TextField juliaXTextField = new TextField(Double.toString(juliaX.get()));
         juliaXTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                Pattern pattern = Pattern.compile("-?[0-9](\\.)");
+                Pattern pattern = Pattern.compile("-?[0-9](\\.)?");
                 Matcher matcher = pattern.matcher(newValue.toString());
                 boolean matchFound = matcher.matches();
                 if (!matchFound) {
@@ -440,7 +440,7 @@ public class FractalApplication extends Application {
         TextField juliaYTextField = new TextField(Double.toString(juliaY.get()));
         juliaYTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                Pattern pattern = Pattern.compile("-?[0-9](\\.)");
+                Pattern pattern = Pattern.compile("-?[0-9](\\.)?");
                 Matcher matcher = pattern.matcher(newValue.toString());
                 boolean matchFound = matcher.matches();
                 if (!matchFound) {
@@ -456,7 +456,7 @@ public class FractalApplication extends Application {
         TextField juliaZoomTextField = new TextField(Double.toString(juliaZoom.get()));
         juliaZoomTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                Pattern pattern = Pattern.compile("-?[0-9](\\.)");
+                Pattern pattern = Pattern.compile("-?[0-9](\\.)?");
                 Matcher matcher = pattern.matcher(newValue.toString());
                 boolean matchFound = matcher.matches();
                 if (!matchFound) {
